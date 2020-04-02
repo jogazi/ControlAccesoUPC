08a7ba4ffc7ab7fa98d14b53dabcd4a5bef14797
jogazi <johan123garciaherran@gmail.com>
2020-04-02 00:33:22 +0000

    Example creation of foreign keys in migrations

---
+++ database/migrations/2020_04_02_001029_pruebaforaneas.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Pruebaforaneas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pruebaforaneas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+	    
+$table->foreign(columns:'campotablareferencia')->references(columns:'iddeestatabla')on->(table:'tabla 
+a referenciar ')onUpdate(action:'restrict')onDelete(action:'restrict');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
